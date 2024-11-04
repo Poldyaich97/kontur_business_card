29eaa22f9b1a9a0199db94ed1bb1d3cc24839ddc
Poldyaich97 <daniil.poldyaev97@gmail.com>
2024-11-04 21:51:38 +0500

    add qr1

--- src/data/users.ts
+++ src/data/users.ts
@@ -5,6 +5,7 @@ export interface User {
   phone: string;
   email: string;
   photo: string;
+  qr: string;
 }
 
 export const users: User[] = [
@@ -14,7 +15,8 @@ export const users: User[] = [
     position: "Руководитель группы",
     phone: "+7 (967) 853-28-13",
     email: "poldyaev@skbkontu.ru",
-    photo: "https://github.com/Poldyaich97/kontur_business_card/blob/main/src/data/1.png?raw=true"
+    photo: "https://github.com/Poldyaich97/kontur_business_card/blob/main/src/data/1.png?raw=true",
+    qr: ""
   },
   {
     id: 2,
